d52a84754049d411199f7c736074a16591ea748b
Rob Lindsey <rob@roblindsey.com>
2020-03-04 18:29:10 -0500

    wip

--- footer.php
+++ footer.php
@@ -14,13 +14,49 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			
-		</div><!-- .site-info -->
+		<div class="footer-wrap">
+			<div class="footer-info">
+				<p>
+					<a href="" class="button">Book Now</a>
+				</p>
+				<p class="m-0">
+					<?php echo date('Y'); ?> Copyright Lasheville, All Rights Reserved.
+				</p>
+				<p class="m-0">
+					Website Design by <a href="https://longlivesimple.com">Long Live Simple.</a>
+				</p>
+			</div>
+			<div class="footer-nav">
+				<ul class="footer-nav-left font-style-heading text-uppercase">
+					<li><a href="<?php echo get_site_url() ?>">Home</a></li>
+					<li><a href="<?php echo get_site_url() ?>/services">Services</a></li>
+					<li><a href="<?php echo get_site_url() ?>/about">About</a></li>
+				</ul>
+				<ul class="footer-nav-right font-style-heading text-uppercase">
+					<li><a href="<?php echo get_site_url() ?>/faq">FAQ</a></li>
+					<li><a href="<?php echo get_site_url() ?>/contact">Contact</a></li>
+					<li><a href="<?php echo get_site_url() ?>/testimonials">Testimonials</a></li>
+				</ul>
+			</div>
+			<div class="footer-social">
+				<h5 class="mt-0">
+					Let's get Social:
+				</h5>
+				<ul>
+					<li><a href="https://www.instagram.com/lashevilleextensions/"><img src="<?php echo get_template_directory_uri() ?>/assets/images/icons/instagram-icon.svg" alt="Instagram"></a></li>
+					<li><a href="https://www.facebook.com/Lasheville?fref=ts"><img src="<?php echo get_template_directory_uri() ?>/assets/images/icons/facebook-icon.svg" alt="Facebook"></a></li>
+					<li><a href="https://www.pinterest.com/umamojo1/"><img src="<?php echo get_template_directory_uri() ?>/assets/images/icons/pinterest-icon.svg" alt="Pinterest"></a></li>
+					<li><a href=""><img src="<?php echo get_template_directory_uri() ?>/assets/images/icons/email-icon.svg" alt="Email"></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer-back-top">
+			<button class="back-top-button">Back To Top</button>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<?php wp_footer();?>
 
 </body>
 </html>
